65b0579b0fb48c9152d270aca1a9ae63f089078a
Benedikt Schulte <benedikt@bullshitmedia.de>
2026-03-08 22:40:13 +0100

    feat: allow SVG uploads and improve SVG handling in media library

--- functions.php
+++ functions.php
@@ -175,6 +175,42 @@ function kuh_get_menus() {
 }
 
 /**
+ * SVG-Upload erlauben
+ */
+function kuh_allow_svg_upload( $mimes ) {
+    $mimes['svg']  = 'image/svg+xml';
+    $mimes['svgz'] = 'image/svg+xml';
+    return $mimes;
+}
+add_filter( 'upload_mimes', 'kuh_allow_svg_upload' );
+
+/**
+ * SVG-Dateityp-Prüfung korrigieren (WordPress erkennt SVG sonst nicht korrekt)
+ */
+function kuh_fix_svg_mime_check( $data, $file, $filename, $mimes ) {
+    $ext = pathinfo( $filename, PATHINFO_EXTENSION );
+    if ( 'svg' === strtolower( $ext ) ) {
+        $data['ext']  = 'svg';
+        $data['type'] = 'image/svg+xml';
+    }
+    return $data;
+}
+add_filter( 'wp_check_filetype_and_ext', 'kuh_fix_svg_mime_check', 10, 4 );
+
+/**
+ * SVG-Vorschau in der Mediathek ermöglichen
+ */
+function kuh_svg_media_preview() {
+    echo '<style>
+        .attachment-266x266, .thumbnail img[src$=".svg"] {
+            width: 100% !important;
+            height: auto !important;
+        }
+    </style>';
+}
+add_action( 'admin_head', 'kuh_svg_media_preview' );
+
+/**
  * REST API erweitern: Featured Image URL zu Posts hinzufügen
  */
 function kuh_register_rest_fields() {
